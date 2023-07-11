18c20f763e8263dc971a5735e18cb077034c8970
gkankith <gkankith@gmail.com>
2023-07-11 14:48:46 +0530

    second commit

--- src/test/java/Wish.java
+++ src/test/java/Wish.java
@@ -4,6 +4,7 @@ public class Wish {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello keep secret");
+		System.out.println("welcome to new world");
 
 	}
 
